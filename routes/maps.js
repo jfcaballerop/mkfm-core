ffb5da12d73e25594cf128031a265cfdb0f64ab0
jfcp <jfcp@inesingenieros.com>
2017-08-23 18:59:59 +0200

    Add files froms disk

--- routes/maps.js
+++ routes/maps.js
@@ -2,11 +2,22 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var config = require(path.join(__dirname, '../config/config'));
+var fs = require('fs');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('maps', { token: req.token, title: config.CLIENT_NAME + '-' + config.APP_NAME, cname: config.CLIENT_NAME, id: req.user_id, login: req.user_login, rol: req.rol, api_key: config.MAPS_API_KEY });
+    var list_files = [];
+    fs.readdir(path.join(__dirname, '../public/uploads'), function(err, files) {
+        if (files)
+            list_files = files.concat();
+        list_files.forEach(function(item) {
+            console.log('## Files: ' + item);
+        });
+        res.render('maps', { files: list_files, token: req.token, title: config.CLIENT_NAME + '-' + config.APP_NAME, cname: config.CLIENT_NAME, id: req.user_id, login: req.user_login, rol: req.rol, api_key: config.MAPS_API_KEY });
+    });
+    console.log('## Files 2: ' + list_files);
+
 });
 
 module.exports = router;
