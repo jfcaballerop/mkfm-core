6ce8d2da84405796bc4ea0c3f04fb79062fffd97
jfcp <jfcp@inesingenieros.com>
2017-09-22 10:08:01 +0200

    Modify info datatrack

--- app/comp/gis/routes/maps.js
+++ app/comp/gis/routes/maps.js
@@ -237,4 +237,58 @@ router.get('/list_files', function(req, resp, next) {
 
 });
 
+/* GET List Info */
+router.get('/list_info', function(req, resp, next) {
+
+    var optionsRoad = {
+        host: config.HOST_API,
+        port: config.PORT_API,
+        path: config.PATH_API + '/infodatatrack/V1/',
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + req.cookies.jwtToken
+        }
+    };
+    var request = http.request(optionsRoad, function(res) {
+        // console.log('STATUS: ' + res.statusCode);
+        // console.log('HEADERS: ' + JSON.stringify(res.headers));
+        res.setEncoding('utf8');
+        var data = '';
+        res.on('data', function(chunk) {
+            var type = typeof chunk;
+            // console.log('BODY: ' + chunk + 'TYPE: ' + typeof chunk);
+            // console.log('TYPE ' + type);
+            data += chunk;
+            // console.log('DATA1 long:: ' + data.length);
+
+
+        });
+        res.on('end', function() {
+            // console.log('DATA2 long ' + data.length);
+            var responseObject = JSON.parse(data);
+            responseObject.forEach(function(item) {
+                delete item["_id"];
+                delete item["updated_at"];
+                delete item["created_at"];
+                //delete item["properties"]["coordTimes"];
+
+            });
+            // responseObject.forEach(function(item) {
+            //     console.log('responseObject.length:: ' + JSON.stringify(item));
+
+            // });
+            // console.log(JSON.stringify(responseObject));
+
+            resp.render('maps', { roadlabs: {}, roads: responseObject, token: req.token, title: config.CLIENT_NAME + '-' + config.APP_NAME, cname: config.CLIENT_NAME, id: req.user_id, login: req.user_login, rol: req.rol, api_key: config.MAPS_API_KEY });
+            //resp.status(200).send(responseObject);
+
+        });
+    });
+
+    request.end();
+    //  resp.render('user', { users: JSON.parse(data), title: config.CLIENT_NAME + '-' + config.APP_NAME, cname: config.CLIENT_NAME, id: req.user_id, login: req.user_login, rol: req.rol });
+
+});
+
 module.exports = router;
