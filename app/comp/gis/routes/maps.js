922da9e9997873fd60f47e163ba7253ff565a72c
jfcp <jfcp@inesingenieros.com>
2017-10-20 11:11:40 +0200

    change tracks info view and review

--- app/comp/gis/routes/maps.js
+++ app/comp/gis/routes/maps.js
@@ -44,6 +44,7 @@ router.get('/', function(req, res, next) {
 
 /* GET List Files */
 router.get('/view_data', function(req, resp, next) {
+    var promises = [];
     var optionsRoad = {
         host: config.HOST_API,
         port: config.PORT_API,
@@ -54,43 +55,83 @@ router.get('/view_data', function(req, resp, next) {
             'Authorization': 'Bearer ' + req.cookies.jwtToken
         }
     };
-    var request = http.request(optionsRoad, function(res) {
-        // console.log('STATUS: ' + res.statusCode);
-        // console.log('HEADERS: ' + JSON.stringify(res.headers));
-        res.setEncoding('utf8');
-        var data = '';
-        res.on('data', function(chunk) {
-            var type = typeof chunk;
-            // console.log('BODY: ' + chunk + 'TYPE: ' + typeof chunk);
-            // console.log('TYPE ' + type);
-            data += chunk;
-            // console.log('DATA1 long:: ' + data.length);
+    var optionsInfoData = {
+        host: config.HOST_API,
+        port: config.PORT_API,
+        path: config.PATH_API + '/infodatatrack/V1/list_order',
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + req.cookies.jwtToken
+        }
+    };
+    promises.push(new Promise(function(resolve, reject) {
+        var localOptions = optionsRoad;
+        var request = http.request(localOptions, function(res) {
+            res.setEncoding('utf8');
+            var data = '';
+            res.on('data', function(chunk) {
+                var type = typeof chunk;
+                data += chunk;
+            });
+            res.on('end', function() {
+                var responseObject = JSON.parse(data);
+                resolve({
+                    hostname: localOptions.hostname,
+                    port: localOptions.port,
+                    path: localOptions.path,
+                    statusCode: res.statusCode,
+                    responseHeaders: JSON.stringify(res.headers),
+                    body: responseObject
+                });
 
-
+            });
         });
-        res.on('end', function() {
-            // console.log('DATA2 long ' + data.length);
-            var responseObject = JSON.parse(data);
-            // responseObject.forEach(function(item) {
-            //     delete item["_id"];
-            //     delete item["updated_at"];
-            //     delete item["created_at"];
-            //     //delete item["properties"]["coordTimes"];
-
-            // });
-            // responseObject.forEach(function(item) {
-            //     console.log('responseObject.length:: ' + JSON.stringify(item));
-
-            // });
-            // console.log(JSON.stringify(responseObject));
-
-            resp.render('maps_data', { utm: utm, roadsvals: responseObject, moment: moment, token: req.token, title: config.CLIENT_NAME + '-' + config.APP_NAME, cname: config.CLIENT_NAME, id: req.user_id, login: req.user_login, rol: req.rol, api_key: config.MAPS_API_KEY });
-            //resp.status(200).send(responseObject);
-
+        request.on('error', function(e) {
+            console.error(e);
+            reject(e);
         });
+        request.end();
+    }));
+
+    promises.push(new Promise(function(resolve, reject) {
+        // save options locally because it will be reassigned to a different object
+        // before it gets used in the callback below
+        var localOptions = optionsInfoData;
+        var req = http.request(localOptions, function(res) {
+            var data = "";
+            res.on('data', function(chunk) {
+                data += chunk;
+            });
+            res.on('end', function() {
+                var responseObject = JSON.parse(data);
+                resolve({
+                    hostname: localOptions.hostname,
+                    port: localOptions.port,
+                    path: localOptions.path,
+                    statusCode: res.statusCode,
+                    responseHeaders: JSON.stringify(res.headers),
+                    body: responseObject
+                });
+            });
+        });
+        req.on('error', function(e) {
+            console.error(e);
+            reject(e);
+        });
+        req.end();
+    }));
+
+
+    // now wait for all promises to be done
+    Promise.all(promises).then(function(allData) {
+        console.log('## maps.js ## Promise.all ##');
+
+        resp.render('maps_data', { utm: utm, roadsvals: allData[0].body, infodatasvals: allData[1].body, moment: moment, token: req.token, title: config.CLIENT_NAME + '-' + config.APP_NAME, cname: config.CLIENT_NAME, id: req.user_id, login: req.user_login, rol: req.rol, api_key: config.MAPS_API_KEY });
+
+    }, function(reason) {
+        return res.status(500).send(reason);
     });
-
-    request.end();
 });
 
 /*
