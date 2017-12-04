466a476255842ffaa1d676a3819e1ec06cd90648
jfcp <jfcp@inesingenieros.com>
2017-12-04 19:02:01 +0100

    modulo de formulas

---
+++ app/services/formulas.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var config = require(path.join(__dirname, '../../config/config'));
+
+exports.criticality = function(type) {
+    console.log('## formulas criticality ##');
+    //console.log(JSON.stringify(arr) + ' ' + index + ' ' + type);
+    var pavementCriticality = {
+        Functionality: {
+            WEIGHT: 0.55,
+            fields: {
+
+            }
+
+
+        }
+    };
+    retJson.geometry.coordinates = arr.geometry.coordinates[index];
+    return pavementCriticality;
+
+}
