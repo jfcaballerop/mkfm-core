bfb4705307d2816f6677be641f4999168e975a0a
jfcaballerop <jfcaballerop@gmail.com>
2018-01-06 01:04:18 +0100

    query not finish

--- app/comp/query/routes/querys.js
+++ app/comp/query/routes/querys.js
@@ -78,7 +78,13 @@ router.get('/consultas', function(req, resp, next) {
     for (var foff of filtersOff) {
         delete filters[foff];
     };
-    resp.render('querys', { filters: filters, token: req.token, moment: moment, title: config.CLIENT_NAME + '-' + config.APP_NAME, cname: config.CLIENT_NAME });
+    resp.render('querys', {
+        filters: filters,
+        token: req.token,
+        moment: moment,
+        title: config.CLIENT_NAME + '-' + config.APP_NAME,
+        cname: config.CLIENT_NAME
+    });
 
     // });
     // });
@@ -130,6 +136,53 @@ router.post('/get_filter_values/:filter', function(req, resp) {
     // resp.status(200).jsonp({});
 
 });
+/* paint_results */
+/**
+ * Proceso AJAX que recibe la peticion de mostrar todos los resultados
+ */
+router.post('/paint_results', function(req, resp) {
+    var postData = extend({}, req.body);
+    debug('## WEB paint_results ' + JSON.stringify(postData));
+
+    var options = {
+        host: config.HOST_API,
+        port: config.PORT_API,
+        path: config.PATH_API + '/query/V1/paint_results/',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(JSON.stringify(postData)),
+            'Authorization': 'Bearer ' + req.cookies.jwtToken
+        }
+    };
+
+
+
+    var request = http.request(options, function(res) {
+        res.setEncoding('utf8');
+        var data = '';
+        res.on('data', function(chunk) {
+            //// debug('BODY: ' + chunk);
+            data += chunk;
+
+        });
+        res.on('end', function() {
+            var responseObject = JSON.parse(data);
+            // debug('\n\nLLEGO AQUI\n\n');
+            resp.status(200).jsonp(responseObject);
+            // resp.status(200).jsonp({ "result": "OK" });
+            // resp.render('querys_results', { results: responseObject, token: req.token, moment: moment, title: config.CLIENT_NAME + '-' + config.APP_NAME, cname: config.CLIENT_NAME });
+
+
+        });
+    });
+    request.on('error', function(err) {
+        debug('problem with request: ${err.message}');
+    });
+    request.write(JSON.stringify(postData));
+    request.end();
+
+});
 
 
 
@@ -168,6 +221,48 @@ router.post('/V1/get_filter_values/:filter', function(req, res, next) {
         //debug(" ### GET Querys ### \n" + JSON.stringify(ifdts));
         ret.filters = filters;
         debug(filters);
+        //res.status(200).jsonp(ifdts);
+        res.status(200).jsonp(ret);
+    });
+
+
+
+});
+
+/* POST paint_results */
+router.post('/V1/paint_results/', function(req, res, next) {
+    // debug('API /V1/update_field/');
+    var postData = extend({}, req.body);
+    debug(postData);
+    var ret = {
+        "result": "OK"
+    };
+    var select = {};
+    var where = {};
+
+    for (var c of postData.columns) {
+        select["properties." + c] = 1;
+    };
+
+    // TODO: falta montar el OR
+    for (var [k, v] of Object.keys(postData).entries()) {
+        debug(k + ' ' + v);
+        if (v !== 'columns') {
+            var inval = { $in: postData[v] };
+            where["properties." + v] = inval;
+        }
+    };
+    debug(select);
+    debug(where);
+    Infodatatrack.find(where, select).exec(function(err, data) {
+        if (err) {
+            ret.result = 'ERROR';
+            ret.errormessage = err.message;
+            res.send(500, ret);
+        }
+        //debug(" ### GET Querys ### \n" + JSON.stringify(ifdts));
+        ret.data = data;
+        // debug(data);
         //res.status(200).jsonp(ifdts);
         res.status(200).jsonp(ret);
     });
