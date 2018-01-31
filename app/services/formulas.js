7f201bef0da03949fa90eb6bd4e26b89e0bd9670
srl <srl@inesingenieros.com>
2018-01-31 17:17:35 +0100

    actualización de la fórmula de criticalidad

--- app/services/formulas.js
+++ app/services/formulas.js
@@ -112,6 +112,66 @@ exports.criticality = function (type, formula, data) {
 
 }
 
+exports.condition = function (type, formula, data) {
+    // console.log('## formulas condition ##');
+    var retCondition = 0;
+
+    //console.log('\n\n------------------------------------------');
+    for (var [l1key, level1] of Object.entries(formula)) {
+        if (typeof level1 === 'object') {
+            //console.log(l1key);
+            var val = 0;
+            for (var [l2key, level2] of Object.entries(level1)) {
+                if (typeof level2 === 'object') {
+                    //console.log(l2key);
+                    if (data[l2key] != undefined && data[l2key] != null) {
+                        if (level2.type === 'select') {
+                            val += level2.scoring[data[l2key]] * level2.weight / 100;
+                        } else if (level2.type === 'range') {
+                            for (var [rk, rango] of Object.entries(level2.scoring)) {
+                                /**
+                                 * compruebo que tenga o no guion para definir el rango y si es el primer o no elemento
+                                 */
+                                // console.log('Rango: ' + rk + ' ' + data[l2key] + ' Scoring ' + level2.scoring[rk]);
+                                if (rk.indexOf('-') == -1) {
+                                    //Ultimo valor del rango
+                                    if (rk * 1.0 <= data[l2key] * 1) {
+                                        // console.log('entro1');
+                                        val += level2.scoring[rk] * level2.weight / 100;
+
+                                    }
+                                } else {
+                                    var valRango = rk.split('-');
+                                    if (valRango[0] * 1.0 < data[l2key] * 1.0 && valRango[1] * 1.0 > data[l2key] * 1.0) {
+                                        // console.log('entro2');
+                                        val += level2.scoring[rk] * level2.weight / 100;
+                                        // console.log('Rango: ' + rk + ' ' + data[l2key] + ' Scoring ' + level2.scoring[rk]);
+                                    }
+                                }
+                            }
+
+                            // console.log('Rango: ' + data[l2key]);
+
+                        }
+                        //console.log('Val: ' + val);
+                    } else {
+                        val += 0;
+                    }
+
+                }
+            }
+            retCondition += val * level1.weight / 100;
+            // console.log('retCriticality: ' + retCriticality);
+        }
+
+    }
+
+
+
+    return retCondition * 100;
+
+}
+
 exports.PavementCost = function (coord1, coord2, rcondrmatcost) {
     var ret = 0;
     var dist = services.calDIST(coord1, coord2);
