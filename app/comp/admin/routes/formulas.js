1f9039f7cec328669e0312b5662e482f28e93365
srl <srl@inesingenieros.com>
2018-02-06 12:31:31 +0100

    rewriting /V1/update_formulas_tracks_condition v6

--- app/comp/admin/routes/formulas.js
+++ app/comp/admin/routes/formulas.js
@@ -829,19 +829,34 @@ router.post('/V1/update_formulas_tracks_condition/:formula/:asset', async functi
                         case 'Culverts':
                             // TODO: calculo de la formula para Pavements -- Sacarlo a un service
                             // debug('form.formulaSpec[f].name' + JSON.stringify(ifdt));
-
+                            var totalScoring = 1E10;
+                            var numberOfScores = 1;
                             for (score in form.formulaSpec[f].MainFactor.Damages.scoring) {
                                 // debug(score.toString.toUpperCase)
                                 if (score !== undefined) {
                                     // debug('score ' + score.toString().toUpperCase());
                                     // debug('ifdt.CDamages ' + score.toString().toUpperCase());
                                     if (ifdt.properties.CDamages.toString().toUpperCase().indexOf(score.toString().toUpperCase()) > 0) {
-                                    debug('score ' + score);
+                                        totalScoring = totalScoring < form.formulaSpec[f].MainFactor.Damages.scoring[score] ?
+                                                        totalScoring : form.formulaSpec[f].MainFactor.Damages.scoring[score];
+                                        numberOfScores++;
+                                        // debug(form.formulaSpec[f].MainFactor.Damages.scoring + ' ' + form.formulaSpec[f].MainFactor.Damages.scoring[score]);
                                     }
                                 }
-                                }
+                            }
 
-                            
+                            totalScoring = (totalScoring == 1E10) ? 0 : totalScoring;
+                            debug(totalScoring);
+
+                            if ( numberOfScores > 2 ) {
+                                totalScoring *= 0.9;
+                            } else {
+                                // existance of several damages
+                                totalScoring *= (-0.1 * numberOfScores)/3+1;
+                            }
+
+
+
 
                             break;
 
