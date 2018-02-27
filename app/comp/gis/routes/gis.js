2b745f2eb6aeb4ff678f96daa91e106bd6c907d0
srl <srl@inesingenieros.com>
2018-02-27 17:55:54 +0100

    file upload v1

--- app/comp/gis/routes/gis.js
+++ app/comp/gis/routes/gis.js
@@ -157,7 +157,75 @@ router.get('/upload', function(req, resp, next) {
     //  resp.render('user', { users: JSON.parse(data), title: config.CLIENT_NAME + '-' + config.APP_NAME, cname: config.CLIENT_NAME, id: req.user_id, login: req.user_login, rol: req.rol });
 
 });
+router.get('/uploadDataSheet', function (req, resp, next) {
+    // Obtengo la lista de extensiones de ficheros
+    var ft_options = {
+        host: config.HOST_API,
+        port: config.PORT_API,
+        path: config.PATH_API + '/filetype/V1/',
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + req.cookies.jwtToken
+        }
+    };
+    var requestft = http.request(ft_options, function (res) {
+        ////// console.log('STATUS: ' + res.statusCode);
+        ////// console.log('HEADERS: ' + JSON.stringify(res.headers));
+        res.setEncoding('utf8');
+        var data = '';
+        res.on('data', function (chunk) {
+            ////// console.log('BODY: ' + chunk);
+            data += chunk;
 
+        });
+        res.on('end', function () {
+            ////// console.log('DATA ' + data.length + ' ' + data);
+            filetypesObject = JSON.parse(data);
+            //resp.render('user', { token: req.token, users: responseObject, title: config.CLIENT_NAME + '-' + config.APP_NAME, cname: config.CLIENT_NAME, id: req.user_id, login: req.user_login, rol: req.rol });
+            //resp.render('upload', { token: req.token, fup: responseObject, moment: moment, title: config.CLIENT_NAME + '-' + config.APP_NAME, cname: config.CLIENT_NAME });
+            //resp.status(200).jsonp(filetypesObject);
+        });
+    });
+    requestft.end();
+
+
+    var options = {
+        host: config.HOST_API,
+        port: config.PORT_API,
+        path: config.PATH_API + '/gis/V1/',
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + req.cookies.jwtToken
+        }
+    };
+    // Peticiones 
+
+
+    var request = http.request(options, function (res) {
+        ////// console.log('STATUS: ' + res.statusCode);
+        ////// console.log('HEADERS: ' + JSON.stringify(res.headers));
+        res.setEncoding('utf8');
+        var data = '';
+        res.on('data', function (chunk) {
+            ////// console.log('BODY: ' + chunk);
+            data += chunk;
+
+        });
+        res.on('end', function () {
+            //// console.log('DATA ' + data.length + ' ' + data);
+            var responseObject = JSON.parse(data);
+            //resp.render('user', { token: req.token, users: responseObject, title: config.CLIENT_NAME + '-' + config.APP_NAME, cname: config.CLIENT_NAME, id: req.user_id, login: req.user_login, rol: req.rol });
+            resp.render('upload', { token: req.token, ft: filetypesObject, fup: responseObject, moment: moment, title: config.CLIENT_NAME + '-' + config.APP_NAME, cname: config.CLIENT_NAME });
+
+        });
+    });
+
+    request.end();
+    //  resp.render('user', { users: JSON.parse(data), title: config.CLIENT_NAME + '-' + config.APP_NAME, cname: config.CLIENT_NAME, id: req.user_id, login: req.user_login, rol: req.rol });
+
+});
 /* UPLOAD File.*/
 var uploading = multer({ dest: path.join(process.env.PWD, '/public/uploads/') }).single('file');
 router.post('/upload', uploading, function(req, resp) {
